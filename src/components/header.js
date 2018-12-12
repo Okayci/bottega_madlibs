def019acc6efd547526b542fc7cc5b11639f0035
Okayci <kayciogden@gmail.com>
2018-12-12 15:46:37 -0700

    initial commit

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = (title, content) => {
+  return (
+    <div className="header">
+      <h1>Bottega Madlibs</h1>
+      <p>
+        Fill out the fields below and click te genereate button to see the Mad
+        Lib Story
+      </p>
+    </div>
+  );
+};
+
+export default Header;
